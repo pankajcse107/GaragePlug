ca71aafd57868a703c9a8961ef901489c5043c5e
pankajcse107 <pankajy14694@gmail.com>
2019-02-07 11:15:33 +0530

    Services added to the User entity

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -10,41 +10,41 @@ import com.example.demo.userepository.UserRepo;
 @RestController
 @RequestMapping("/customer1")
 public class UserController {
-    private UserRepo userRepo;
+
     private final UserService userService;
 
 @Autowired
-public UserController(UserRepo userRepo, UserService userService)
+public UserController( UserService userService)
 {
-    this.userRepo = userRepo;
+
     this.userService = userService;
 }
 
 @PostMapping()
   public User createCostumer(@RequestBody User customer)
  {
-    return userRepo.save(customer);
+    return userService.save(customer);
  }
  @RequestMapping(method = RequestMethod.GET)
     public  Iterable<User> get()
   {
-        return userRepo.findAll();
+        return userService.findAll();
   }
   @RequestMapping(path = "/{id}",method = RequestMethod.GET)
     public User getDetails(@PathVariable("id") Long id)
   {
-      return userRepo.findById(id).get();
+      return userService.findById(id);
   }
-  @RequestMapping(value = "na",method = RequestMethod.GET)
+  /*@RequestMapping(value = "na",method = RequestMethod.GET)
     public int getOrderNo(@RequestParam (value = "name") String name ,@RequestParam(value = "id") Long id)
   {
-      return userRepo.findByNameAndId(name,id).getNo_of_orders();
-  }
-  @RequestMapping(value = "status",method = RequestMethod.GET)
-          public String getStatus(@RequestParam (value = "name") String name,@RequestParam(value = "id") Long id)
+      return userService.findByNameAndId(name,id).getNo_of_orders();
+  }*/
+  /*@RequestMapping(value = "status",method = RequestMethod.GET)
+  public String getStatus(@RequestParam (value = "name") String name,@RequestParam(value = "id") Long id)
   {
-      return userRepo.findByNameAndId(name,id).getCustomer_Status();
-  }
+      return userService.findByNameAndId(name,id).getCustomer_Status();
+  }*/
 
   @DeleteMapping("/delete")
   public void deleteAll()
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,7 +1,10 @@
 package com.example.demo.service;
 
+import com.example.demo.customer.User;
 import com.example.demo.userepository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
 
 @org.springframework.stereotype.Service
 public class UserService
@@ -23,5 +26,17 @@ public class UserService
     public void putStatus()
     {
 
+    }
+
+    public User save(User customer) {
+         return userRepo.save(customer);
+    }
+
+    public Iterable<User> findAll() {
+        return userRepo.findAll();
+    }
+
+    public User findById(Long id) {
+        return userRepo.findById(id).get();
     }
 }
